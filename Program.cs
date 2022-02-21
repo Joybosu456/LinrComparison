19e43b107a1e14e2e4c1dd26ae06fd2e4aa271a1
JOY <joybosu@gmail.com>
2022-02-21 20:46:31 +0530

    Main mathod or welcome

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Problem");
+            CartesianSystem.Length();
+            CheckEqulity.CheckEqulityOfLine();
+            GraterOrLess.GraterOrLessThen();
+        }
+    }
+}
